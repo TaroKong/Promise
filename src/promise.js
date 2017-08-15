70597819c2e4fb8981b950cb806888d795f42e69
江天德 <tarojiang@tencent.com>
2017-08-15 16:05:37 +0800

    调整目录结构

--- src/promise.js
+++ src/promise.js
@@ -3,7 +3,6 @@
  */
 
 ( function ( global, factory ) {
-
     'use strict';
 
     if ( typeof module === 'object' && typeof module.exports === 'object' ) {
@@ -11,9 +10,7 @@
     } else {
         factory( global );
     }
-
 } )( typeof window !== 'undefined' ? window : this, function ( window, noGlobal, undefined ) {
-
     var PENDING = 'pending', // 进行中
         RESOLVED = 'resolved', // 已完成
         REJECTED = 'rejected'; // 已失败
@@ -52,7 +49,6 @@
                                 window.MozMutationObserver;
 
         try {
-
             var counter = 1,
                 textNode = document.createTextNode( counter ),
                 observer = new MutationObserver( nextTickHandler );
@@ -65,11 +61,8 @@
                 counter = (counter + 1) % 2;
                 textNode.data = counter;
             };
-
         } catch ( e ) {
-
             timerFunc = window.setImmediate || setTimeout;
-
         }
 
         return function ( cb, ctx ) {
@@ -91,55 +84,40 @@
     }());
 
     Promise.fn = Promise.prototype = {
-
         constructor: Promise,
-
         then: function (onFullfilled, onRejected) {
-
             var _this = this,
                 Promise = this.constructor,
                 args = arguments;
 
             return Promise(function (resolve, reject) {
-
                 var fnMap = [ '__onFullfilled', '__onRejected' ],
                     i = 0,
                     l = 2;
 
                 for ( ; i < l; i++ ) {
-
                     _this[ fnMap[ i ] ] = (function ( fn ) {
-
                         return function ( val ) {
                             var isFun = isFunction( fn ),
                                 ret = isFun ? fn( val ) : val;
 
                             if ( isPromise( ret ) ) {
-
                                 ret.then(
                                     function ( val ) {
-
                                         if ( !isFun ) {
                                             reject( ret );
                                         } else {
                                             // TODO 是否存在 PromiseStatus 为 resolved 且 PromiseValue 为 Promise 实例的情况?
                                         }
-
                                     }, function ( val ) {
-
                                         reject( !isFun ? ret : val );
-
                                     }
                                 );
-
                             } else {
                                 ( isFun || _this.PromiseStatus === RESOLVED ? resolve : reject )( ret );
                             }
-
                         };
-
                     })( args[ i ] || undefined );
-
                 }
 
                 if ( _this.PromiseStatus === RESOLVED ) {
@@ -155,9 +133,7 @@
                 }
 
                 args = null;
-
             });
-
         },
         "catch": function ( onRejected ) {
             return this.then( null, onRejected );
@@ -189,7 +165,6 @@
         },
         // 私有方法
         __resolve: function ( val ) {
-
             if ( this.PromiseStatus === PENDING ) {
                 this.PromiseStatus = RESOLVED;
                 this.PromiseValue = val;
@@ -200,11 +175,9 @@
                     }, this);
                 }
             }
-
         },
         // 私有方法
         __reject: function ( val ) {
-
             if ( this.PromiseStatus === PENDING ) {
                 this.PromiseStatus = REJECTED;
                 this.PromiseValue = val;
@@ -219,7 +192,6 @@
     };
 
     Promise.all = function ( iterator ) {
-
         var Promise = this;
 
         return Promise(function (resolve, reject) {
@@ -227,7 +199,6 @@
             if ( !isArray( iterator ) ) {
                 reject( 'TypeError: (var)[Symbol.iterator] is not a iterator.' );
             } else {
-
                 var i = 0,
                     l = iterator.length,
                     count = 0,
@@ -235,25 +206,20 @@
                     result = [];
 
                 for ( ; i < l; i++ ) {
-
                     if ( rejected ) {
                         break;
                     }
 
                     (function ( i, p ) {
-
                         Promise.resolve( p ).then(
                             function ( val ) {
-
                                 result[ i ] = val;
 
                                 if( ++count >= l ) {
                                     resolve( result );
                                     count = null;
                                 }
-
                             }, function ( val ) {
-
                                 if ( rejected ) {
                                     return false;
                                 }
@@ -262,22 +228,15 @@
 
                                 rejected = true;
                                 count = null;
-
                             }
                         );
-
                     } ( i, iterator[i] ));
-
                 }
-
             }
-
         });
-
     };
 
     Promise.resolve = function ( x ) {
-
         if ( isPromise( x ) ) { // Promise 实例
             return x;
         } else {
@@ -289,11 +248,9 @@
                         }
                 );
         }
-
     };
 
     Promise.reject = function ( r ) {
-
         if ( isPromise( r ) ) { // Promise 实例
             return r;
         } else {
@@ -305,11 +262,9 @@
                     }
             );
         }
-
     };
 
     var init = Promise.fn.init = function ( executor ) {
-
         if ( typeof executor !== 'function' ) {
             throw new Error( 'Promise resolver is not a function' );
         }
@@ -341,7 +296,6 @@
         } catch ( error ) {
             _this.__reject( error );
         }
-
     };
 
     init.prototype = Promise.fn;
@@ -357,5 +311,4 @@
     }
 
     return Promise;
-
 } );
